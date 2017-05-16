c4a6157af5319210b7c2840356aca616115fa178
DivFloat <1468573677@qq.com>
2017-05-16 15:27:31 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $(function(){
 	var h=$(window).height();
-	$(".content-wrap").css("height",h*0.278+"px");
+	$(".content-wrap").css("height",h*0.5+"px");
 	var w1=$("#img").width();
 	$("#img").css("height",w1);
 });
