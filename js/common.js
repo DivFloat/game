05f4bf42234f7586fe849faa99de1e2916e0fd26
DivFloat <1468573677@qq.com>
2017-05-17 00:23:59 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -6,10 +6,19 @@ $(function(){
 	$("body").css("height",h);
 	var w1=$("#img").width();
 	$("#img").css("height",w1);
-	var w2=$("#d1-img").width();
-	$("#d1-img").css("height",w2);
+	var h2=$("#d1-img").height();
+	$("#d1-img").css("width",h2);
+	var h4=$("#d2-img").height();
+	$("#d2-img").css("width",h4);
 	var h1=$("#hotgame").height();
 	$("#hotgame").css("lineHeight",h1+"px");
 	var h3=$("#icon").height();
 	$("#icon").css("marginTop",h3*(-0.5)+"px");
+	var w2=$(".d1").width();
+	$(".d1 h2,.d2 h2,.d2 p,.d1 p").css("left",w2*0.08+h2+"px");
+	var w3=$(".d1 h2").width();
+	$(".d1 ul,.d2 ul").css("marginLeft",w3+w2*0.04+"px");
+	var w4=$(".d1 span").width();
+	$(".d1 span,.d2 span").css("marginLeft",w3+h2/2+w2*0.02+w4+"px");
 });
+
