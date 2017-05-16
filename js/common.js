2595618d5af1584a5678b96912f2a17815253cce
DivFloat <1468573677@qq.com>
2017-05-16 13:59:23 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -3,25 +3,25 @@ $(function(){
 	var h = $(window).height();
 	$("header").css("height",h*0.067);
 	$("#hotgame").css({
-		"width":'w*0.3',
-		"height":'h*0.067',
-		"lineHeight": 'h*0.067'+"px",
-		"fontSize": 'h*0.03'		
+		"width":w*0.3+"px",
+		"height":h*0.067+"px",
+		"lineHeight": h*0.067+"px",
+		"fontSize": h*0.03+"px"		
 	});
 	$("#icon").css({
-		"fontSize":'h*0.048',
-		"marginLeft":'w*0.047'
+		"fontSize":h*0.048+"px",
+		"marginLeft":w*0.047+"px"
 	});
 	$(".content-wrap").css({
-		"height":'h*0.278',
-		"padding-top":'h*0.068'
+		"height":h*0.278+"px",
+		"padding-top":h*0.068+"px"
 	});
 	$("#img").css({
-		"width":'w*0.157',
-		"height":'w*0.157',
+		"width":w*0.157+"px",
+		"height":w*0.157+"px",
 	});
 	$("p:first").css({
-		"fontSize":'h*0.035',
-		"marginTop":'h*0.02'
+		"fontSize":h*0.035+"px",
+		"marginTop":h*0.02+"px"
 	});
 })
