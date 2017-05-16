bbd8c5bcb1462c0cb16aa040639764682f240ae6
DivFloat <1468573677@qq.com>
2017-05-16 15:16:27 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,8 +1,4 @@
 $(function(){
-	var ah=$("#icon").height();
-	$("#icon").css("lineHeight",ah+"px");
-	var gh=$("#hotgame").height();
-	$("#hotgame").css("lineHeight",gh+"px");
 	var w1=$("#img").width();
 	$("#img").css("height",w1);
 });
